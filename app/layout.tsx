0000157e470a0738b70f5ad07d40ae28434e9256
Murali Vara Prasad <muralivaraprasad8@gmail.com>
2026-06-12 09:05:36 +0530

    Added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "../app/components/Navbar";
+import Footer from "./components/Footer";
 
 export default function RootLayout({
   children,
@@ -14,6 +15,7 @@ export default function RootLayout({
 
         {children}
 
+        <Footer />
       </body>
     </html>
   );
